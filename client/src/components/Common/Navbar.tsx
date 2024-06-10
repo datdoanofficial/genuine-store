c4811a5fa0cd8ce0ad166e11f31aac0a59b50a21
DAT DOAN <datdoangongzuo@gmail.com>
2024-06-10 18:20:38 +0700

    add sticky navbar

--- client/src/components/Common/Navbar.tsx
+++ client/src/components/Common/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../Common/Logo";
 import PrimaryBtn from "../Common/PrimaryBtn";
@@ -8,8 +8,23 @@ import "./Navbar.scss";
 type Props = {};
 
 const Navbar = (props: Props) => {
+  const navbarRef = useRef<HTMLDivElement>(null); // Use useRef to reference the navbar div
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (navbarRef.current) {
+        navbarRef.current.classList.toggle("sticky", window.scrollY > 0);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    // Cleanup function to remove the event listener
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="navbar">
+    <div ref={navbarRef} className="navbar">
       <div className="navbar-link">
         <Link to="/">
           <Logo />
@@ -29,7 +44,6 @@ const Navbar = (props: Props) => {
           </li>
         </ul>
       </div>
-
       <div className="navbar-featured">
         <Link to="/cart">
           <span className="solar--bag-3-outline shopping-bag"></span>
